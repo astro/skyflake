1b9736170aff8744e42683b7365a93e4dad15384
Astro <astro@spaceboyz.net>
2022-12-22 02:17:32 +0100

    vm/nomad-job: reenable virtiofsd killpriv-v2

--- vm/nomad-job.nix
+++ vm/nomad-job.nix
@@ -148,7 +148,7 @@ ${''
     --socket-path=${socket} \
     --socket-group=kvm \
     --shared-dir=${source} \
-    --sandbox=none --no-killpriv-v2 \
+    --sandbox=none \
     --xattr --posix-acl \
     --inode-file-handles=prefer \
     --cache=never
